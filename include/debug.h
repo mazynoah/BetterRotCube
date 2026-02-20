2caf824d8c3473306348d2dac029090ef1fd17ed
chalettt <wyatt.rossi@epita.fr>
2026-02-21 00:38:12 +0100

    fix(main): automatic \n when logging.

--- include/debug.h
+++ include/debug.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 // Macros for logs and errors output.
-#define ERROR(FORMAT, ...) fprintf(stderr, "cube: *** " FORMAT, ##__VA_ARGS__)
-#define LOG(FORMAT, ...) fprintf(stdout, "cube: " FORMAT, ##__VA_ARGS__)
+#define ERROR(FORMAT, ...) fprintf(stderr, "cube: *** " FORMAT "\n", ##__VA_ARGS__)
+#define LOG(FORMAT, ...) fprintf(stdout, "cube: " FORMAT "\n", ##__VA_ARGS__)
 
 #endif /* ! DEBUG_H */
