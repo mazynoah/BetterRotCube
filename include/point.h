197c71debb18fcb33c13d7d5f211a0c9ddb55cf9
chalettt <wyatt.rossi@epita.fr>
2026-02-21 00:39:59 +0100

    feat(main): Added x axis rotation and change the projection method to account for camera rotation.

--- include/point.h
+++ include/point.h
@@ -1,3 +1,6 @@
+#ifndef POINT_H
+#define POINT_H
+
 typedef enum direction
 {
   LEFT,
@@ -16,5 +19,12 @@ typedef struct point
 Point *create_point(double x, double y, double z);
 Point *add_point(Point *a, Point *b);
 Point *sub_point(Point *a, Point *b);
+Point *dup_point(Point *p);
 Point *add_dir(Point *p, Direction d, double delta);
+Point *project(Point* point);
+Point *rotate_point_y(Point *point, Point *origin, double alpha);
+Point *rotate_point_x(Point *point, Point *origin, double alpha);
+void dump_point(Point *point);
+
+#endif /* POINT_H */
 
--- src/point.c
+++ src/point.c
@@ -1,7 +1,11 @@
 #include "point.h"
 #include "debug.h"
+#include "sdl_manager.h"
+#include "camera.h"
+#include "vector.h"
 
 #include <stdlib.h>
+#include <math.h>
 
 Point *create_point(double x, double y, double z)
 {
@@ -34,6 +38,11 @@ Point *sub_point(Point *a, Point *b)
   return a;
 }
 
+Point *dup_point(Point *p)
+{
+  return create_point(p->x, p->y, p->z);
+}
+
 Point *add_dir(Point *p, Direction d, double delta)
 {
   switch (d)
@@ -59,5 +68,84 @@ Point *add_dir(Point *p, Direction d, double delta)
   }
 
   return p;
+}
+
+Point* project(Point* point)
+{
+  double fov = 90.0;
+  double d = WIDTH/2*tan(fov/2);
+
+  // HANK DO NOT ABREVIATE CAMERA POSITION TO CP!
+  Point *cp = dup_point(camera->position);
+  Point *cl = dup_point(camera->look_ahead);
+
+  Point *forward = vector_normalize(sub_point(cl, cp));
+  Point *word_up = create_point(0, 1, 0);
+  Point *cross = vector_cross(word_up, forward);
+  Point *right = vector_normalize(cross);
+  Point *up = vector_cross(forward, right);
+
+  Point *p = dup_point(point);
+  Point *rel = sub_point(p, cp);
+
+  double x = dot_product(rel, right);
+  double y = dot_product(rel, up);
+  double z = dot_product(rel, forward);
+
+  free(cl);
+  free(forward);
+  free(word_up);
+  free(cross);
+  free(right);
+  free(up);
+  free(p);
+
+  if (z < 0.01)
+    return NULL;
+
+  double projection_x = d * x / z + WIDTH / 2;
+  double projection_y = HEIGHT / 2 - d * y / z;
+
+  return create_point(projection_x, projection_y, z);
+}
+
+// Rotates a point by an angle alpha along the Y axis according to the center
+// of the space.
+Point *rotate_point_y(Point *point, Point *origin, double alpha)
+{
+  double x = point->x - origin->x;
+  double y = point->y - origin->y;
+  double z = point->z - origin->z;
+
+  double new_x = x * cos(alpha) + z * sin(alpha);
+  double new_z = -x * sin(alpha) + z * cos(alpha);
+
+  point->x = new_x + origin->x;
+  point->y = y + origin->y;
+  point->z = new_z + origin->z;
+
+  return point;
+}
+
+// Rotates a point by an angle alpha along the X axis according to the origin point.
+Point *rotate_point_x(Point *point, Point *origin, double alpha)
+{
+  double x = point->x - origin->x;
+  double y = point->y - origin->y;
+  double z = point->z - origin->z;
+
+  double new_y = y * cos(alpha) - z * sin(alpha);
+  double new_z = y * sin(alpha) + z * cos(alpha);
+
+  point->x = x + origin->x;
+  point->y = new_y + origin->y;
+  point->z = new_z + origin->z;
+  
+  return point;
+}
+
+void dump_point(Point *point)
+{
+  LOG("Point(x: %f; y: %f; z: %f)\n", point->x, point->y, point->z);
 }
 
